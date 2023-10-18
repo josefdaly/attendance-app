b25600277c86ef1dafbb4e04a3d31abdbd9bbbb7
josefdaly <josefdaly@gmail.com>
2023-10-17 20:20:54 -0700

    change templates for heroku

--- attendance/attendance/settings.py
+++ attendance/attendance/settings.py
@@ -85,11 +85,7 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if IS_HEROKU_APP:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
-    # https://github.com/jazzband/dj-database-url
+    TEMPLATES[0]['DIRS'] = ['attendance/templates']
     DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
